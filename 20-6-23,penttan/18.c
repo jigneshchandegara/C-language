aed35f37be327003b82c03ae966c5687317951ae
jigneshchandegara <135798255+jigneshchandegara@users.noreply.github.com>
2023-08-05 15:32:52 +0530

    Add files via upload

---
+++ 20-6-23,penttan/18.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	int i,j;
+	
+	for(i=2; i<=6; i++){
+		for(j=2; j<=i; j++){
+			printf("%d ",j%2);
+		}
+		printf("\n");
+	}
+	return 0;
+}
---
+++ 20-6-23,penttan/22.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+	char i,j,count='A';
+	
+	for(i='A'; i<='E'; i++){
+		for(j='A'; j<=i; j++){
+			printf("%c ",count++);
+		}
+		printf("\n");
+	}
+	return 0;
+}
