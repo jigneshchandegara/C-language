f18d2fd86d6d26eaf94d350fab876bd4b83d8ccd
jigneshchandegara <135798255+jigneshchandegara@users.noreply.github.com>
2023-08-05 15:35:22 +0530

    Add files via upload

---
+++ 31-5-23,penttan/1.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+int main(){
+	printf("name : chandegara jignesh govindbhai  \nage :22 \ncollage :s.v.patel");
+	return 0 ;
+}
---
+++ 31-5-23,penttan/3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+	printf(" - - -\n");
+	printf("j     |\n");
+	printf("i     |\n");
+	printf("g     |\n");
+	printf("n     |\n");
+	printf("e     |\n");
+	printf("s     |\n");
+	printf("h     |\n");
+	printf(" - - -\n");
+	return 0 ;
+}
